a557ff1fb8d44da89a861863613933c34b7ebcdf
Moosa Raza <razasyedmoosa882@gmail.com>
2025-06-30 00:24:35 +0500

    Updated code of navbar and home page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
     return (
         <div className='fixed top-0 left-0 z-50 w-full flex items-center
-            justify-between px-6 md:px-16 lg:px-36 py-5 bg-black'>
+            justify-between px-6 md:px-16 lg:px-36 py-5 bg-transparent'>
             <Link to='/' className='max-md:flex-1'>
                 <img src={assets.logo} alt='' className='w-36 h-auto' />
             </Link>
