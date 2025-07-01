b90df2f70d97d8ab2065d9c2557073f98feffbae
Moosa Raza <razasyedmoosa882@gmail.com>
2025-07-02 01:08:05 +0500

    Pushed Trailer Section and added in Home Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import FeaturedSection from '../components/FeaturedSection'
+import TrailerSection from '../components/trailerSection'
 
 const Home = () => {
   return (
     <div>
       <HeroSection/>
       <FeaturedSection/>
+      <TrailerSection/>
+      
         
     </div>
   )
