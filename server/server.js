49940aa9024f2c8b24b1eb064ce61bf09d30e098
Moosa Raza <razasyedmoosa882@gmail.com>
2025-07-25 23:28:42 +0500

    merging updated files

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import showRouter from './routes/showRoutes.js';
 
 const app=express();
 const port=3000;
@@ -18,6 +19,7 @@ app.use(clerkMiddleware())
 //API ROUTES
 app.get('/',(req,res)=> res.send('Server is Live!!'))
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use('/api/show',showRouter)
 
 app.listen(port,()=> console.log(`Server Listening At http://localhost:${port}`))
 
