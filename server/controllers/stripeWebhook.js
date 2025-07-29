0ce43e38b5b5f23608c4fe4c2173a20fd9d35c09
Moosa Raza <razasyedmoosa882@gmail.com>
2025-07-30 00:50:45 +0500

    Stripe Integration

---
+++ server/controllers/stripeWebhook.js
@@ -0,0 +1,42 @@
+import stripe from "stripe";
+import Booking from "../models/Booking.js";
+
+export const stripeWebhook = async (request, response) => {
+    const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
+    const sig = request.headers['striep-signature'];
+    let event;
+    try {
+        event = stripeInstance.webhooks.constructEvent(request.body, sig, process.
+            env.STRIPE_WEBHOOK_SECRET)
+    }
+    catch (error) {
+        return response.status(400).send(`Webhook Error: ${error.message}`);
+    }
+
+    try {
+        switch (event.type) {
+            case "payment_intent.succeeded": {
+                const paymentIntent = event.data.object
+                const sessionList = await stripeInstance.checkout.sessions.list({
+                    payment_intent: paymentIntent.id
+                })
+                const session = sessionList.data[0]
+                const { bookingId } = session.metadata
+                await Booking.findByIdAndUpdate(bookingId,{
+                    isPaid:true,
+                    paymentLink:""
+                })
+                break;
+            }
+            default:
+                console.log('Unhandled Event Type:',event.type)
+        }
+        res.json({received:true})
+
+    } catch (err) {
+        console.error("Webhook Processing Error:",err);
+        res.json(500).send("Internal Server Error")
+
+    }
+
+}
