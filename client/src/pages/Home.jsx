10cf6e67960d879a7f801c5df84cdbe377ef7206
Moosa Raza <razasyedmoosa882@gmail.com>
2025-08-01 00:22:14 +0500

    Done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import FeaturedSection from '../components/FeaturedSection'
-import TrailerSection from '../components/trailerSection'
+import TrailerSection from '../components/TrailerSection'
 
 const Home = () => {
   return (
